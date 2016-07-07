43b3bfcb39e23c96ec42655d8002c6e6ed732500
Prateek Vasireddy <prat.vasireddy@gmail.com>
2016-07-07 19:20:48 +0000

    done

--- code.js
+++ code.js
@@ -1,1 +1,17 @@
 'use strict';
+function favIceCream(type){
+    return "I love " + type;
+}
+
+function shouting(input){
+    return input.toUpperCase();
+}
+
+function roundDown(num){
+    return Math.floor(num);
+}
+
+function theTruth(){
+    return true;
+}
+
